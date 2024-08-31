762d6e0d22ac065ae34bd3bebbe8d26c4dfc66a7
dj <darrellbrand@gmail.com>
2024-08-30 18:47:30 -0700

    cleaned

--- app/src/main/java/com/example/recipeapp/data/data_source/remote/ApiService.kt
+++ app/src/main/java/com/example/recipeapp/data/data_source/remote/ApiService.kt
@@ -2,15 +2,8 @@ package com.example.recipeapp.data.data_source.remote
 
 import com.example.recipeapp.domain.model.CategoryResponse
 import com.example.recipeapp.domain.model.MealResponse
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
-
-private val retrofit = Retrofit.Builder().baseUrl("http://www.themealdb.com/api/json/v1/1/")
-    .addConverterFactory(GsonConverterFactory.create()).build()
-
-val recipeService: ApiService? = retrofit.create(ApiService::class.java)
 
 interface ApiService {
     @GET("random.php")
--- app/src/main/java/com/example/recipeapp/data/data_source/remote/OpenAIService.kt
+++ app/src/main/java/com/example/recipeapp/data/data_source/remote/OpenAIService.kt
@@ -3,16 +3,12 @@ package com.example.recipeapp.data.data_source.remote
 import com.example.recipeapp.BuildConfig
 import com.example.recipeapp.domain.model.ApiKeyResponse
 import com.example.recipeapp.domain.model.OpenAiResponse
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Headers
+import retrofit2.http.POST
 import retrofit2.http.Query
 
-private val retrofit = Retrofit.Builder().baseUrl("https://spring-backend-ghcr-latest.onrender.com")
-    .addConverterFactory(GsonConverterFactory.create()).build()
-val openAiService: OpenAiService? = retrofit.create(OpenAiService::class.java)
 private const val apiKeyHeader = BuildConfig.xapikey
 private const val androidIdHeader = BuildConfig.xandroidid
 private const val basicAuthHeader = BuildConfig.xbasicauth
@@ -22,7 +18,7 @@ interface OpenAiService {
     @Headers("Authorization: Basic $basicAuthHeader")
     suspend fun getApiKey(@Header(androidIdHeader) androidId: String): ApiKeyResponse
 
-    @GET("ai/generate")
+    @POST("ai/generate")
     @Headers("Authorization: Basic $basicAuthHeader")
     suspend fun getOpenAIRecipe(
         @Header(androidIdHeader) androidId: String,
